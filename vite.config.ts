86b105a844ae17defad64b338aa3d1dae046acbf
Alina Maslova <missis_maslina@mail.ru>
2025-11-01 13:17:23 +0300

    ь

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,8 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', // Изменяем на относительный путь
+  base: '',
+  build: {
+    outDir: 'docs'
+  }
 })
